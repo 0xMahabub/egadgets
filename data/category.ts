6017be71c6ad5d34d5c89e4f83ffa8c0ea225229
0xMahabub <mahabub212002072@gmail.com>
2022-12-12 15:32:35 +0600

    mock-data --added

---
+++ data/category.ts
@@ -0,0 +1,33 @@
+interface ICat {
+  id: number;
+  name: string;
+  code: string;
+}
+
+export const categories: ICat[] = [
+  {
+    id: 1,
+    name: 'Smartphone',
+    code: 'smartphone',
+  },
+  {
+    id: 2,
+    name: 'iPhone',
+    code: 'iphone',
+  },
+  {
+    id: 3,
+    name: 'Android',
+    code: 'android',
+  },
+  {
+    id: 4,
+    name: 'Laptop',
+    code: 'laptop',
+  },
+  {
+    id: 5,
+    name: 'Macbook',
+    code: 'macbook',
+  },
+];

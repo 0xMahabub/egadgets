f12edcb482d87e199d122776841a8ea012dd872a
0xMahabub <mahabub212002072@gmail.com>
2022-12-14 18:06:13 +0600

    products:ui:started

--- src/components/ListProductsGrid.tsx
+++ src/components/ListProductsGrid.tsx
@@ -1,5 +1,16 @@
 import { FC } from 'react';
 import { IProductItems } from '../interfaces';
-export const ListProductsGrid: FC<IProductItems> = () => {
-  return <p>List {`:=>`} Products @Grid</p>;
+export const ListProductsGrid: FC<IProductItems> = ({ items }) => {
+  return (
+    <>
+      <div className='grid gap-5 grid-cols-3'>
+        {items?.map((it, i) => (
+          <div key={i} className='p-4 shadow-md rounded-md'>
+            <p>{it.name}</p>
+            <small>{`$` + it.price}</small>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
